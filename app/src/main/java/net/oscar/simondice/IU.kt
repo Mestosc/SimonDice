5d70d7d634ffb522766236423661bf346fd76868
Oscar <oscarodriguez56@gmail.com>
2025-11-05 13:09:47 +0100

    Añadiendo la funcion para dibujar los botones normales del juego al adivinar

--- app/src/main/java/net/oscar/simondice/IU.kt
+++ app/src/main/java/net/oscar/simondice/IU.kt
@@ -1,2 +1,20 @@
 package net.oscar.simondice
 
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+
+@Composable
+fun IU() {
+
+}
+
+@Composable
+fun BotonesNormales(modeloVista: ModeloVista,color: Colores) {
+    val activo = modeloVista.estadoActual.collectAsState().value.boton_activo
+    Button(onClick = {modeloVista.incrementandoLista(color)}, enabled = activo, colors = ButtonDefaults.buttonColors(color.color)) {
+        Text(color.txt)
+    }
+}
