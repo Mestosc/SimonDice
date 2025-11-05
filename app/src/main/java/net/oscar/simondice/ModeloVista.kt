b0b819b70fce615f802fe41a0d48849f1bb080fd
Oscar <oscarodriguez56@gmail.com>
2025-11-05 12:24:56 +0100

    Empezando a trabajar en el ModelView y el README

--- app/src/main/java/net/oscar/simondice/ModeloVista.kt
+++ app/src/main/java/net/oscar/simondice/ModeloVista.kt
@@ -1,7 +1,9 @@
 package net.oscar.simondice
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class ModeloVista : ViewModel() {
+    var secuenciaAdivinar: MutableStateFlow<List<Colores>> = MutableStateFlow(mutableListOf());
 
 }
