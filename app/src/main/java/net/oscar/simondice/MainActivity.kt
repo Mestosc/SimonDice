f3131d76acfc7ae6365481f9542b5353cb683231
Oscar <oscarodriguez56@gmail.com>
2025-11-05 13:15:33 +0100

    Añadiendo el modeloVista a la MainActivity.kt

--- app/src/main/java/net/oscar/simondice/MainActivity.kt
+++ app/src/main/java/net/oscar/simondice/MainActivity.kt
@@ -17,15 +17,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val modeloVista = ModeloVista()
         setContent {
-            SimonDiceTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            IU(modeloVista)
         }
     }
 }
