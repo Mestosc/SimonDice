9865be52830d216689aed623ff569ea88a040c78
Oscar <oscarodriguez56@gmail.com>
2025-11-11 11:47:00 +0100

    Añadiendo sistema de puntuacion, y otras cosas

--- app/src/main/java/net/oscar/simondice/ModeloVista.kt
+++ app/src/main/java/net/oscar/simondice/ModeloVista.kt
@@ -6,18 +6,22 @@ import kotlinx.coroutines.flow.MutableStateFlow
 
 class ModeloVista : ViewModel() {
     var estadoActual = MutableStateFlow(Estados.INICIO)
+    var puntuacion = MutableStateFlow(0)
     private val tagLOG = "ModeloDebug"
 
     /**
      * Añado un nuevo [color] a la lista de la secuencia que yo estoy poniendo
      */
     fun incrementandoLista(color: Colores) {
+        if (finalizoJuego(color)) {
+            estadoActual.value = Estados.FINALIZANDO
+        }
         if (Datos.secuenciaAdivinando.size<Datos.secuenciaAdivinar.size) {
             Log.d(tagLOG,"Añadiendo color ${color.color} a la secuencia")
             Datos.secuenciaAdivinando.add(color)
-        }
-        if (finalizoJuego(color)) {
-            estadoActual.value = Estados.FINALIZANDO
+        } else {
+            puntuacion.value += 1
+            estadoActual.value = Estados.ADIVINAR
         }
     }
 
