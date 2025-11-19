c459c6d5a28f0a211a24613a6e7e7372ac0606fc
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-11-19 10:12:54 +0100

    Refactor: Mueve la limpieza de secuencia a estado GENERANDO y añade logs de inicio de estado.

--- app/src/main/java/net/oscar/simondice/Estados.kt
+++ app/src/main/java/net/oscar/simondice/Estados.kt
@@ -22,6 +22,12 @@ sealed class Estados(val modeloVista: ModeloVista) {
     class GENERANDO(modeloVista: ModeloVista) : Estados(modeloVista) {
         override fun onEnter() {
             Log.d(tagLOG,"Iniciando estado $this")
+            if (!Datos.secuenciaAdivinando.isEmpty()) {
+                Log.d(tagLOG,"Limpiando secuencia existente")
+                Datos.secuenciaAdivinando.clear() /* Nos aseguramos de que en cada
+            ronda la secuencia que componemos al pulsar los botones de la interfaz en este caso
+            este vacia para que al intentar adivnar la lista en cada ronda no genere problemas*/
+            }
             boton_activo = false
             start_activo = false
             Datos.secuenciaAdivinar.forEach { v -> Log.d("Color",v.txt) }
@@ -33,6 +39,7 @@ sealed class Estados(val modeloVista: ModeloVista) {
     }
     class JUGANDO(modeloVista: ModeloVista) : Estados(modeloVista) {
         override fun onEnter() {
+            Log.d(tagLOG,"Iniciando estado $this")
             boton_activo = true
             start_activo = false
         }
@@ -42,14 +49,9 @@ sealed class Estados(val modeloVista: ModeloVista) {
     }
     class FINALIZANDO(modeloVista: ModeloVista) : Estados(modeloVista) {
         override fun onEnter() { // Funcion que se ejecuta al entrar en el estado de Finalizacion
+            Log.d(tagLOG,"Inciando estado $this")
             boton_activo = false
             start_activo = true
-            if (!Datos.secuenciaAdivinando.isEmpty()) {
-                Log.d(tagLOG,"Limpiando secuencia existente")
-                Datos.secuenciaAdivinando.clear() /* Nos aseguramos de que en cada
-            ronda la secuencia que componemos al pulsar los botones de la interfaz en este caso
-            este vacia para que al intentar adivnar la lista en cada ronda no genere problemas*/
-            }
             modeloVista.puntuacion.value = 0 // Haciendo que si fallas y acaba el juego se reinicie la puntuacion
         }
 
