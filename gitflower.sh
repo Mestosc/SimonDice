37145c5cae2c461cf46e976b00972bf25c79a62d
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-11-25 12:44:59 +0100

    Eliminar basura del gitflower

--- gitflower.sh
+++ gitflower.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
 
 # Facilita el hacer mergear cosas en la feature y volver inmediatamente a la personal
-_flowmerge() {
-  local cur branches
-  cur="${COMP_WORDS[COMP_CWORD]}"
-  branches=$(git branch --format='%(refname:short)' 2>/dev/null)
-  COMPREPLY=( $(compgen -W "$branches" -- "$cur") )
-}
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-  complete -F _flowmerge ./gitflower.sh
-fi
 git checkout "$1";git merge "$2";git checkout "$2"
