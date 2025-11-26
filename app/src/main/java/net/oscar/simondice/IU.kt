c97a8866e420b54ebac7206e34d36a3a6978edb6
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-11-26 12:49:43 +0100

    Refactor: Limpiar imports de IU y ajustar comentarios en `IU.kt`.

--- app/src/main/java/net/oscar/simondice/IU.kt
+++ app/src/main/java/net/oscar/simondice/IU.kt
@@ -2,27 +2,16 @@ package net.oscar.simondice
 
 import android.content.Context
 import android.media.MediaPlayer
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -126,7 +115,7 @@ fun BotonesNormales(modeloVista: ModeloVista,color: Colores) {
     Button(onClick = { // Se intento implementar la logica de sonido buscada pero no se logro
         mediaPlayer.start()
         modeloVista.botonIluminado.value = color
-        kotlinx.coroutines.CoroutineScope(kotlinx.coroutines.Dispatchers.Main).launch {
+        kotlinx.coroutines.CoroutineScope(kotlinx.coroutines.Dispatchers.Main).launch { // Esta corrutina es para que cuando pulse el boton
             kotlinx.coroutines.delay(300)
             modeloVista.botonIluminado.value = null
         }
