813a9cc3c86e99113a82d1d7e5bda04e694f4e19
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-12-03 11:47:21 +0100

    Refactor: Eliminar parámetro 'key' de obtenerRecord en PuntuacionMasAltaHandler.

--- app/src/main/java/net/oscar/simondice/puntuacionMasAlta/PuntuacionMasAltaHandler.kt
+++ app/src/main/java/net/oscar/simondice/puntuacionMasAlta/PuntuacionMasAltaHandler.kt
@@ -6,7 +6,7 @@ import net.oscar.simondice.Datos.PuntuacionMasAlta
  * Guardar los records en el juego del simon dice
  */
 interface PuntuacionMasAltaHandler {
-    fun obtenerRecord(key: String): PuntuacionMasAlta;
+    fun obtenerRecord(): PuntuacionMasAlta;
     fun anadirRecord(puntuacionMasAlta: PuntuacionMasAlta);
     fun eliminarRecord(puntuacionMasAlta: PuntuacionMasAlta);
 }
