c51675728b637d168c5a5a5a441ba3f9ccb92a88
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-12-03 11:31:10 +0100

    Refactor: Limpiar GreetingPreview de código obsoleto.

--- app/src/main/java/net/oscar/simondice/MainActivity.kt
+++ app/src/main/java/net/oscar/simondice/MainActivity.kt
@@ -35,8 +35,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    val modeloVista = ModeloVista()
     SimonDiceTheme {
-        IU(modeloVista)
+
     }
 }
