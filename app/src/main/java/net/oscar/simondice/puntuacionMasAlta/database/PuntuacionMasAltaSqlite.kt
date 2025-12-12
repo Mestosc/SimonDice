30eb2611795120e0ee70164d76b8bed716d7e121
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-12-12 10:55:52 +0100

    feat: Implementada la obtención del récord de puntuación desde SQLite

--- app/src/main/java/net/oscar/simondice/puntuacionMasAlta/database/PuntuacionMasAltaSqlite.kt
+++ app/src/main/java/net/oscar/simondice/puntuacionMasAlta/database/PuntuacionMasAltaSqlite.kt
@@ -7,13 +7,38 @@ import android.database.sqlite.SQLiteOpenHelper
 import net.oscar.simondice.datos.ConstantesVarias
 import net.oscar.simondice.datos.PuntuacionMasAlta
 import net.oscar.simondice.puntuacionMasAlta.PuntuacionMasAltaHandler
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class PuntuacionMasAltaSqlite(context: Context, val formatter: DateTimeFormatter = ConstantesVarias.DEFAULT_FORMATTER) : PuntuacionMasAltaHandler {
     val db = BaseDatosHelper(context)
     override fun obtenerRecord(): PuntuacionMasAlta {
         val dbReader = db.readableDatabase
+        val projection = arrayOf(DataBaseContract.TablaRecord.COLUMNA_RECORD, DataBaseContract.TablaRecord.COLUMNA_FECHA)
 
+
+// How you want the results sorted in the resulting Cursor
+        val sortOrder = "${DataBaseContract.TablaRecord.COLUMNA_RECORD} DESC"
+
+        val cursor = dbReader.query(
+            DataBaseContract.TablaRecord.TABLE_NAME,
+            projection,
+            null,
+            null,
+            null,                   // don't group the rows
+            null,                   // don't filter by row groups
+            sortOrder               // The sort order
+        )
+        var puntuacion = 0
+        var fecha = "12/12/2012 12:12"
+        with(cursor) {
+            if (moveToNext()) {
+                puntuacion = getInt(getColumnIndexOrThrow(DataBaseContract.TablaRecord.COLUMNA_RECORD))
+                fecha = getString(getColumnIndexOrThrow(DataBaseContract.TablaRecord.COLUMNA_FECHA))
+            }
+        }
+        cursor.close()
+        return PuntuacionMasAlta(puntuacion, LocalDateTime.parse(fecha,formatter))
     }
 
     override fun anadirRecord(puntuacionMasAlta: PuntuacionMasAlta) {
