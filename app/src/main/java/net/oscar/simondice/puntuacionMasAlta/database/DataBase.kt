242af2c8c871f8785d66c09d70b353f478d2429e
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2026-02-04 11:05:57 +0100

    Cambiar la version en la clase que representa la base de datos para que funcione

--- app/src/main/java/net/oscar/simondice/puntuacionMasAlta/database/DataBase.kt
+++ app/src/main/java/net/oscar/simondice/puntuacionMasAlta/database/DataBase.kt
@@ -3,7 +3,7 @@ package net.oscar.simondice.puntuacionMasAlta.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database([RecordSimon::class],version = 1, exportSchema = false)
+@Database([RecordSimon::class],version = 2, exportSchema = false)
 abstract class DataBase : RoomDatabase() {
     abstract fun recordDao(): RecordDAO
 }
