99a660d4577d25ae1f9cabb7f6d6c5c2c6684d96
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2026-02-04 10:42:45 +0100

    Añadiendo nombre a los dos modelos de la estructura

--- app/src/main/java/net/oscar/simondice/datos/PuntuacionMasAlta.kt
+++ app/src/main/java/net/oscar/simondice/datos/PuntuacionMasAlta.kt
@@ -7,4 +7,4 @@ import java.time.format.DateTimeFormatter
  * Representa la puntuacion más alta
  */
 data class PuntuacionMasAlta(var puntuacionMasAlta: Int = ConstantesVarias.DEFAULT_SCORE, var marcaTiempo: LocalDateTime = LocalDateTime.parse(
-    ConstantesVarias.DEFAULT_DATE_STRING, ConstantesVarias.DEFAULT_FORMATTER))
+    ConstantesVarias.DEFAULT_DATE_STRING, ConstantesVarias.DEFAULT_FORMATTER), val nombre: String = "Jugador1")
--- app/src/main/java/net/oscar/simondice/puntuacionMasAlta/database/RecordSimon.kt
+++ app/src/main/java/net/oscar/simondice/puntuacionMasAlta/database/RecordSimon.kt
@@ -6,5 +6,6 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class RecordSimon(@PrimaryKey(autoGenerate = true) val uid: Int?,
+                       @ColumnInfo val nombre: String?,
                        @ColumnInfo("record") val record: Int?,
                        @ColumnInfo val fecha: String?)
